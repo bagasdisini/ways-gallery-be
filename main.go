8a2ed047fe5219b6d351b340076281d783ccef3b
Bagas <mbagas221@gmail.com>
2022-11-09 23:03:24 +0700

    update

--- main.go
+++ main.go
@@ -34,9 +34,9 @@ func main() {
 
 	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
 
-	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "*"})
+	var AllowedHeaders = handlers.AllowedHeaders([]string{"Access-Control-Allow-Origin", "*", "X-Requested-With", "Content-Type", "Authorization", "Origin", "Accept"})
 	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
-	var AllowedOrigins = handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	var AllowedOrigins = handlers.AllowedOrigins([]string{"http://localhost:3006"})
 
 	fmt.Println("Running in localhost:5000")
 	http.ListenAndServe(":"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
